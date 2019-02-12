78bca98561c83961298e571e0610a443ce2ac7a8
amekyun <amekyun@163.com>
2019-02-12 06:23:37 -0500

    1

---
+++ Wang_PE8/Wang_PE8/Potion.cs
@@ -0,0 +1,60 @@
+﻿//Jixian Wang
+//Class name: Potion
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wang_PE8
+{
+    class Potion
+    {
+        //parameter for this class
+        string color;
+        double strength;
+        string name;
+        string usage;
+        int doses;
+
+        //constructor
+        public Potion(string pColor,double pStrength,string pName,string pUsage,int pDoses)
+        {
+            color = pColor;
+            strength = pStrength;
+            name = pName;
+            usage = pUsage;
+            doses = pDoses;
+        }
+
+        //overload constructor
+        public Potion()
+        {
+            name = "Potion";
+            strength = 50;
+            color = "colorless";
+            usage = "To cast a person";
+            doses = 10;
+        }
+
+        //use potion
+        public void UsePotion()
+        {
+            //doses -1
+            doses--;
+            Console.WriteLine("Using potion... {0} doses left.", doses);
+        }
+
+        //print out 
+        public void PrintPotion()
+        {
+            Console.WriteLine("Potion:");
+            Console.WriteLine("\tName: {0}",name);
+            Console.WriteLine("\tStength: {0}", strength);
+            Console.WriteLine("\tColor: {0}", color);
+            Console.WriteLine("\tUsage: {0}", usage);
+            Console.WriteLine("\tDoses: {0}", doses);
+
+        }
+    }
+}
---
+++ Wang_PE8/Wang_PE8/Program.cs
@@ -0,0 +1,31 @@
+﻿//Jixian Wang
+//Wang_PE8
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wang_PE8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create new potion 
+            Potion myCustomPotion = new Potion("green", 78.20, "Speed Potion", "Make people faster", 7);
+            //print
+            myCustomPotion.PrintPotion();
+
+            //create default potion
+            Potion defaultPotion = new Potion();
+            //print
+            defaultPotion.PrintPotion();
+
+            //use potion
+            myCustomPotion.UsePotion();
+            //print out
+            myCustomPotion.PrintPotion();
+        }
+    }
+}
